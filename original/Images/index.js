6221c44c0e296e2367978e9e74c295a8ef079b0a
Juan Ignacio Galán <juanigalan91@gmail.com>
2019-03-01 16:17:56 -0300

    first iteration created

--- original/Images/index.js
+++ original/Images/index.js
@@ -3,28 +3,33 @@ import PropTypes from 'prop-types';
 
 const namespace = 'images';
 const Images = (props) => {
-    const { images } = props;
+    const { images, title } = props;
     return (
-        <ul className={namespace}>
-            {
-                images.map(image => (
-                    <li className="image">
-                        <a href={image.link} className={`${namespace}__link`}>
-                            <img
-                                src={image.src}
-                                alt={image.title}
-                                className={`${namespace}__img`}
-                            />
-                        </a>
-                    </li>
-                ))
-            }
-        </ul>
+        <div className={namespace}>
+            <h1 key="title" className={`${namespace}__title`}>{title}</h1>
+            <ul key={namespace} className={`${namespace}__list`}>
+                {
+                    images.map(image => (
+                        <li className="image" key={image.key}>
+                            <a href={image.link} className={`${namespace}__link`} target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src={image.src}
+                                    alt={image.title}
+                                    className={`${namespace}__img`}
+                                />
+                                <h3 className={`${namespace}__img_title`}>{image.title}</h3>
+                            </a>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     );
 };
 
 Images.propTypes = {
-    images: PropTypes.array.isRequired
+    images: PropTypes.array.isRequired,
+    title: PropTypes.string.isRequired
 };
 
 export default Images;
