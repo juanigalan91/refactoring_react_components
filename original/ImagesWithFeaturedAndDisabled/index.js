49e105194dab719c0c84177dcc150189a37fdd34
Juan Ignacio Galán <juanigalan91@gmail.com>
2019-03-01 16:50:59 -0300

    add featured and disabled case

--- original/ImagesWithFeaturedAndDisabled/index.js
+++ original/ImagesWithFeaturedAndDisabled/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const namespace = 'images';
 const Images = (props) => {
@@ -11,9 +12,13 @@ const Images = (props) => {
                 {
                     images.map((image) => {
                         const baseClassName = 'image';
+                        const className = classNames(baseClassName, {
+                            [`${baseClassName}__featured`]: image.isFeatured,
+                            [`${baseClassName}__disabled`]: image.isDisabled,
+                        });
 
                         return (
-                            <li className={baseClassName} key={image.key}>
+                            <li className={className} key={image.key}>
                                 <a href={image.link} className={`${baseClassName}__link`} target="_blank" rel="noopener noreferrer">
                                     <img
                                         src={image.src}
