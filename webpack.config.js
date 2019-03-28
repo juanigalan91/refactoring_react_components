f0cc69a8735ed233d3df927e3b15dbf69b6d15f6
Juan Ignacio Galán <juanigalan91@gmail.com>
2019-03-28 14:46:47 -0300

    remove redundant rule from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,11 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
     template: path.join(__dirname, 'original/index.html'),
     filename: './index.html'
 });
+
 module.exports = {
     entry: path.join(__dirname, '/original/index.js'),
     module: {
         rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
